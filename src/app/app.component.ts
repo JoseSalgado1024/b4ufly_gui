636f995eadbecc09e919d730ee59a14131707783
Jose A. Salgado <jsalgado@HP-EA014.anac.local>
2018-04-23 15:13:29 -0300

    Actualizo presentacion de datos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,67 +8,100 @@ import { DataAcquireService } from './data-acquire.service';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
-  public initial_lat = -34.573878;
-  public initial_lng = -58.488616;
-  public initial_zoom = 12;
-  buffer_radius = 5000;
-  public airports_nearest = [];
+  // APP setting
+  public title = 'app';
+  public is_loading = true;
+
+  // Data Containers
+  public near_objects_warning = [];
+  public near_objects_denied = [];
   public airports = [];
+
+  // Data Filters & Defaults
+  public lat = -37.259949;
+  public lng = -65.564819;
+  public zoom = 10;
+  public near_airports_warning_distance = 5000;
+  public near_airports_denied_distance = 1000;
+  public viewport_range = 200000;
+  public lisftoff_time = '0';
 
   constructor (private _airports: DataAcquireService) {
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition( (position) => {
-        this.initial_lat = position.coords.latitude;
-        this.initial_lng = position.coords.longitude;
-        this.initial_zoom = 13;
-        console.log('Latitude: ' + this.initial_lat + ' and Longitude: ' + this.initial_lng);
+        this.lat = position.coords.latitude;
+        this.lng = position.coords.longitude;
+        this.zoom = 16;
+        console.log('Latitude: ' + this.lat + ' and Longitude: ' + this.lng);
       });
     }
   }
 
-  AirportsNearest() {
-    // console.log();
-    this._airports.getAirPortsNearest(this.initial_lat, this.initial_lng, this.buffer_radius).subscribe(
+  private getNearetObjectsWarning() {
+    this._airports.getNearAirports(this.lat, this.lng, this.near_airports_warning_distance).subscribe(
       data => {
-        // console.log(data.results);
-        data.results.forEach(element => {
-          this.airports_nearest.push(
-            {
-              lat: element.the_geom.properties.gg_point_coordinates[0],
-              lng: element.the_geom.properties.gg_point_coordinates[1],
-              name: element.the_geom.properties.name,
-              local_identifier: element.local_identifier
-            });
-        });
+        for (let wa of data.results ) {
+          if (this.near_objects_denied.indexOf(wa.local_identifier) < 0) {
+            this.near_objects_warning.push(wa.local_identifier);
+          }
+        }
       },
-      err => console.error(err),
-      () => console.log('Successful Airports load!')
+      err => console.log(err),
+      () => console.log('Nearest Objects (Warning) successful loaded!.')
     );
   }
 
-  Airports() {
-    // console.log();
-    this._airports.getAirPortslist().subscribe(
+  private getNearetObjectsDenied() {
+    this._airports.getNearAirports(this.lat, this.lng, this.near_airports_warning_distance).subscribe(
       data => {
-        // console.log(data.results);
-        data.results.forEach(element => {
-          this.airports.push(
-            {
-              lat: element.the_geom.properties.gg_point_coordinates[0],
-              lng: element.the_geom.properties.gg_point_coordinates[1],
-              name: element.the_geom.properties.name,
-              local_identifier: element.local_identifier
-            });
-        });
+        for (let da of data.results ) {
+            this.near_objects_warning.push(da.local_identifier);
+        }
       },
-      err => console.error(err),
-      () => console.log('Successful Airports load!')
+      err => console.log(err),
+      () => console.log('Nearest Objects (Denied) successful loaded!.')
     );
   }
 
-  lookUpFeatures (event) {
-    console.log(event);
+  getAirportsData() {
+    this.near_objects_warning = [];
+    this.near_objects_denied = [];
+    this.getNearetObjectsDenied();
+    this.getNearetObjectsWarning();
+    this._airports.getNearAirports(this.lat, this.lng, this.viewport_range).subscribe(
+    data => {
+      for (let a of data.results ) {
+        this.airports.push({
+          lat: a.the_geom.properties.gg_point_coordinates[0],
+          lng: a.the_geom.properties.gg_point_coordinates[1],
+          local: a.local_identifier,
+          name: a.human_readable_identifier,
+          warning: this.near_objects_denied.indexOf(a.local_identifier) < 0,
+          denied: this.near_objects_denied.indexOf(a.local_identifier) < 0
+        });
+      }
+      if ( this.near_objects_warning.length > 0 ) {
+        this.lisftoff_time = '1';
+      } else if ( this.near_objects_denied.length > 0 ) {
+        this.lisftoff_time = '2';
+      }
+
+    },
+    err => console.log(err),
+    () => {
+            this.is_loading = false;
+            console.log('Airports Data Layer successful loaded.');
+          }
+    );
+  }
+
+  pickPosition(event) {
+    this.lat = event.coords.lat;
+    this.lng = event.coords.lng;
+    this.near_objects_denied = [];
+    this.near_objects_warning = [];
+    this.getAirportsData();
+    console.log('Moved to lat: ' + this.lat + ', lng: ' + this.lng);
   }
 }
 
